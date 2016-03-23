e08fb652461d61dc96c94aac6e732db3ccb2b871
nmcintyre2014 <nmcintyr@DGNathanMcIntyre.local>
2016-03-22 22:07:25 -0600

    type in run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ java -jar builds/image-processing-stuff-1.0.jar
 java -Xmx1024M -jar builds/image-processing-stuff-1.0.jar -if test_data/image_coordinates.csv -id DSC02698.JPG -of /tmp/DSC02698_out.tif
 
 # Process the second image
-java -Xmx1024M -jar builds/image-processing-stuff-1.0.jar -if test_data/image_coordinates.csv -id DSC03449.JPG -nt 8 -of /tmp/DSC03449_out.tif
+java -Xmx1024M -jar builds/image-processing-stuff-1.0.jar -if test_data/image_coordinates.csv -id DSC03449.JPG -of /tmp/DSC03449_out.tif
 
 # Process the second image again, this time using only one thread
 java -Xmx1024M -jar builds/image-processing-stuff-1.0.jar -if test_data/image_coordinates.csv -id DSC03449.JPG -nt 1 -of /tmp/DSC03449_out.tif
